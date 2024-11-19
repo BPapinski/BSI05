0c2d1d14bbdf6c956924450a9095957975ec0dc4
Bartek <bartosz.papinski00@gmail.com>
2024-11-20 00:12:11 +0100

    deleted unnecessary files

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -7,9 +7,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="aboutStyle.css">
-    <link rel="stylesheet" href="reset.css">
-
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -65,7 +62,7 @@
 
 </head>
 <body>
-    <?php // nie trzeba domykac bo caly plik bedzie php     
+    <?php  
         session_start();
         
         if (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true) {
